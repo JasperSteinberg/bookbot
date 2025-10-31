561b888bfbbb5a5489ea9f0f0f37bd349891bc34
JasperSteinberg <jaspersteinberg2@gmail.com>
2025-10-31 13:47:43 +0100

    inital book logic

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(FILE_PATH: str) -> str:
+    with open(FILE_PATH) as file:
+        file_contents = file.read()
+    
+    return file_contents
+
+def word_count(text: str) -> int:
+    return len(text.split())
+
+def main():
+    content = get_book_text("books/frankenstein.txt")
+    count = word_count(content)
+    print(f"Found {count} total words")
+
+main()
